7c8d88d38e24dfa319dd0c79b3fa2f53ba8670cb
Fern <fernando.c.arimori@gmail.com>
2026-08-24 16:22:08 -0300

    update study day3

--- day3/remove_dupe_ex.py
+++ day3/remove_dupe_ex.py
@@ -14,3 +14,6 @@ df_responde
 df_responde.shape
 #%%
 df_responde["IdCustomer"].nunique()
+#%%
+df[df["IdCustomer"] == "5f8fcbe0-6014-43f8-8b83-38cf2f4887b3"].sort_values(by="DtTransaction", ascending=False)
+df_responde[df_responde["IdCustomer"] == "5f8fcbe0-6014-43f8-8b83-38cf2f4887b3"]
