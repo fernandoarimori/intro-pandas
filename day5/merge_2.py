d0d5f767e712373f7197970c4b212261ead1a847
Fern <fernando.c.arimori@gmail.com>
2026-08-31 15:46:16 -0300

    start day6

--- day5/merge_2.py
+++ day5/merge_2.py
@@ -1,30 +1,39 @@
 #%%
 import pandas as pd
 
-dt_user = pd.read_csv("../data/customers.csv", sep=";")
-dt_user
+df_user = pd.read_csv("../data/customers.csv", sep=";")
+df_user
 
 #%%
-dt_transaction = pd.read_excel("../data/transactions.xlsx")
-dt_transaction
+df_transaction = pd.read_excel("../data/transactions.xlsx")
+df_transaction
 #%%
-dt_transaction_product = pd.read_parquet("../data/")
+df_transaction_product = pd.read_parquet("../data/transactions_cart.parquet")
+df_transaction_product
 
 #%%
-dt_transaction.merge(
-    dt_user,
+df_merged_transaction_user_product = df_transaction.merge(
+    df_user,
     how="inner",
     left_on= "IdCustomer",
     right_on="UUID",
     suffixes=["_transaction", "_customer"]
-)
+).merge(
+    df_transaction_product,
+    how="left",
+    left_on="UUID_transaction",
+    right_on="IdTransaction"
+    )
 
 
 # %%
-checking =dt_transaction.merge(
-    dt_user,
+checking =df_transaction.merge(
+    df_user,
     how="left",
     left_on= "IdCustomer",
     right_on="UUID"
 ).copy()
 checking[checking["UUID_y"].isna()]
+checking
+#%%
+df_merged_transaction_user_product 
